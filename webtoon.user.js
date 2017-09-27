28f58edd93c13d588a8c2099fa64d0a85ef783ef
maxzerr <11054956@students.southwales.ac.uk>
2017-09-27 23:32:24 +0530

    Uploaded JS Installation file

---
+++ webtoon.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Webtoon Scrolling Lag Bug Fix
+// @namespace    https://github.com/Crack/webtoons.com-scroll-killer
+// @description  Restores normal behaviour of down and up keys on webtoons.com
+// @version      1.0
+// @match        http://www.webtoons.com/*
+// @grant        none
+// ==/UserScript==
+javascript:(function(){oRecentWebtoon = null;})();
